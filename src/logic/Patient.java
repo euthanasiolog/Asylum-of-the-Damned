0a9c24b45deafa526d8d4afdde994dec2363e915
euthanasiolog <piatryxa@gmail.com>
2016-12-19 19:51:40 +0300

    поехали!!

---
+++ src/logic/Patient.java
@@ -0,0 +1,11 @@
+package logic;
+
+/**
+ * Created by Piatrok on 19.12.2016.
+ * в этом классе описываем пациента, как нетрудно догадаться
+ */
+public class Patient {
+    String name;
+    int age;
+    
+}
