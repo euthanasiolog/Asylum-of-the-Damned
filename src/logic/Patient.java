e838b0deadeafef1026b8a45526c26bb14d0b2b9
plizzzhealme <plizzz.healme@gmail.com>
2016-12-21 08:57:59 +0300

    поля класса пациент

--- src/logic/Patient.java
+++ src/logic/Patient.java
@@ -1,21 +1,39 @@
 package logic;
 
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * в этом классе описываем пациента, как нетрудно догадаться
  */
 public class Patient {
+
+    // Поля, задающиеся при создании объекта
     private String name; //имя
     private String surName; // фамилия
     private String patronymic; // отчество
     private String address;
     private LocalDate birthday;
-    private int age; // высчитывается из даты рождения
     private Sex gender; // пол
     private LocalDate hospitalizationDate; // с какого числа лежит
     private String diagnosis; // диагноз (пока просто строка)
     private boolean isHospitalized; // true - госпитализирован, false - дома
-    private int ward; // палата
 
+    // Задаются в процессе работы
+    private int ward; // палата (можно также добавить отделение)
+    private List<Recipe> drugs; // список принимаемых лекарств
+
+    public Patient(String name, String surName, String patronymic,
+                   String address, LocalDate birthday, Sex gender,
+                   LocalDate hospitalizationDate, String diagnosis) {
+        this.name = name;
+        this.surName = surName;
+        this.patronymic = patronymic;
+        this.address = address;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.hospitalizationDate = hospitalizationDate;
+        this.diagnosis = diagnosis;
+        isHospitalized = true;
+    }
 }
---
+++ src/logic/Recipe.java
@@ -0,0 +1,12 @@
+package logic;
+
+import java.time.LocalDate;
+
+/**
+ * Рецепт
+ */
+public class Recipe {
+    private String drug; // название препарата (или процедуры)
+    private LocalDate begin;
+    private LocalDate end;
+}
